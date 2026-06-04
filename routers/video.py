63c18c58f8891b3a5d19dc6b4f1e5f1be61e3bb8
Peter <peter279k@gmail.com>
2026-06-04 20:07:51 +0800

    Remove /video/ API route support

--- routers/video.py
+++ routers/video.py
@@ -14,47 +14,6 @@ async def get_api_version():
         'version': '0.2.0',
     }
 
-async def extract_video_info(video_url: str = ''):
-    ydl_opts = {
-        'quiet': True,
-        'simulate': True,
-    }
-
-    response = {'error': None}
-
-    parsed_url_result = urlparse(video_url)
-    if parsed_url_result.netloc != 'www.youtube.com':
-        response['error'] = 'Unsupported %s!' % video_url
-
-        return response
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info = ydl.extract_info(video_url)
-
-            response = {'links': []}
-            for format_lists in info['formats']:
-                if format_lists.get('acodec') is None:
-                    continue
-                if format_lists['audio_ext'] == 'm4a' and 'videoplayback?' in format_lists['url']:
-                    response['links'].append({
-                        'format': format_lists['ext'],
-                        'itag': format_lists['resolution'] + '(' + str(format_lists['aspect_ratio']) + ')',
-                        'url': format_lists['url'],
-                    })
-                elif format_lists['video_ext'] == 'mp4' and 'videoplayback?' in format_lists['url']:
-                    response['links'].append({
-                        'format': format_lists['ext'],
-                        'itag': format_lists['resolution'] + '(' + str(format_lists['aspect_ratio']) + ')',
-                        'url': format_lists['url'],
-                    })
-
-        except Exception as e:
-            response['error'] = str(e)
-
-
-    return response
-
 async def download_video(url: str = Query(...)) -> dict:
     job_id = str(uuid.uuid4())
     download_video_task.delay(url, job_id)
