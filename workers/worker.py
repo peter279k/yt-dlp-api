ef56a9cf1977bd81ec4381a0ad791b8b560e9a83
Peter <peter279k@gmail.com>
2026-08-18 22:49:09 +0800

    Change into chromium

--- workers/worker.py
+++ workers/worker.py
@@ -17,7 +17,7 @@ celery = Celery(
 
 def get_cookie_from_playwright():
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=True)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         )
